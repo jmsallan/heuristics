f6837403169bed42c03d1aaca1beb149d370dbf3
Jose M Sallan <jose.maria.sallan@upc.edu>
2015-10-12 18:57:02 +0200

    QAP code
    Added the code to do the QAP assignment

---
+++ QAPstart.R
@@ -0,0 +1,38 @@
+#-----reading the Elshafei:77 file as an example---- 
+
+download.file(url="http://anjos.mgi.polymtl.ca/qaplib/data.d/els19.dat", "Elsh19.dat")
+
+data <- scan("Elsh19.dat")
+
+n <- data[1]
+
+flow <- matrix(data[2:(n*n + 1)], n, n)
+
+distance <- matrix(data[(n*n + 2):(2*n*n + 1)], n , n)
+
+optimum <- c(9,10,7,18,14,19,13,17,6,11,4,5,12,8,15,16,1,2,3)
+value_optimum <- 17212548
+
+#-------defining objective function------- 
+
+
+perMatrix <- function(vec){
+  n <- length(vec)
+  m <- matrix(0, n , n)
+  for(i in 1:n){
+    m[i, vec[i]] <- 1
+  }
+  return(m)
+}
+
+objective <- function(flow, distance, permutation){
+  pmatrix <- perMatrix(permutation)
+  dist_perm <- pmatrix %*% distance %*% t(pmatrix)
+  return(sum(flow * dist_perm))
+}
+
+#---------testing objective function------
+
+obj.value <- objective(flow, distance, optimum)
+
+obj.value == value_optimum
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # heuristics
+
+This is the gitHub repository of heuristics of Jose M Sallan. Here you will find quite simple heuristics to attack combinatorial problems, as the code here is set for teaching purposes.
